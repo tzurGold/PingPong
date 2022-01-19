ccb4b0c6c78e8a0141a811282178d31ddc41571a
Tzur Goldstein <tzurgol@alonn.tzafonet.org.il>
2022-01-19 15:27:23 +0200

    Add projects

---
+++ PingPong/Server.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Server.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
