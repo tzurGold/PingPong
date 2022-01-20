ab650c8fbc83c850a10b5037a1ffb2f700e33f9f
Tzur Goldstein <tzurgold10@gmail.com>
2022-01-20 11:24:02 +0200

    Implement socket server factory

--- PingPong/Server.BLL/Implementations/SocketServerFactory.cs
+++ PingPong/Server.BLL/Implementations/SocketServerFactory.cs
@@ -1,12 +1,13 @@
 ﻿using Server.BLL.Abstraction;
+using UI.Implementations;
 
 namespace Server.BLL.Implementations
 {
     public class SocketServerFactory : IServerFactory
     {
-        public ServerBase CreateServer(int port)
+        public ServerBase CreateServer(int port, NotifyException notifyException, IAction action)
         {
-            return new SocketServer(port);
+            return new SocketServer(port, notifyException, action);
         }
     }
 }
